f8eb723f2cf3ba2e919216fbb335861efc0920c2
briannasullivan <113055705+briannasullivan@users.noreply.github.com>
2022-09-19 21:01:57 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,46 @@
-let hand = 0
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
 input.onGesture(Gesture.Shake, function () {
-    hand = randint(0, 2)
-    if (hand == 0) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    } else if (hand == 1) {
-        basic.showLeds(`
-            # # . . #
-            # # . # .
-            . . # . .
-            # # . # .
-            # # . . #
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
+    PickShape()
+})
+function PickShape () {
+    hand = randint(1, 3)
+    if (hand == rockIndex) {
+        rock.showImage(0)
     }
-})
-basic.forever(function () {
-	
-})
+    if (hand == paperIndex) {
+        paper.showImage(0)
+    }
+    if (hand == ScissorsIndex) {
+        scissors.showImage(0)
+    }
+}
+let hand = 0
+let scissors: Image = null
+let paper: Image = null
+let rock: Image = null
+let ScissorsIndex = 0
+let paperIndex = 0
+let rockIndex = 0
+rockIndex = 1
+paperIndex = 2
+ScissorsIndex = 3
+rock = images.createImage(`
+    . . . . .
+    . # # # .
+    # # # # #
+    # # # # #
+    . . . . .
+    `)
+paper = images.createImage(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+scissors = images.createImage(`
+    # # . . #
+    # # . # .
+    . . # . .
+    # # . # .
+    # # . . #
+    `)
