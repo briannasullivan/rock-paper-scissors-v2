9996c824c1c8b0c423641d0f77511839934a10f4
briannasullivan <113055705+briannasullivan@users.noreply.github.com>
2022-09-20 22:37:40 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
-input.onGesture(Gesture.Shake, function () {
-    PickShape()
+radio.onReceivedNumber(function (receivedNumber) {
+    handplayer2 = receivedNumber
 })
-function PickShape () {
+input.onButtonPressed(Button.A, function () {
+    if (handplayer2 == rockIndex) {
+        rock.showImage(0)
+    }
+    if (handplayer2 == paperIndex) {
+        paper.showImage(0)
+        if (handplayer2 == ScissorsIndex) {
+            scissors.showImage(0)
+        }
+    }
+})
+input.onGesture(Gesture.Shake, function () {
     hand = randint(1, 3)
     if (hand == rockIndex) {
         rock.showImage(0)
@@ -12,14 +23,17 @@ function PickShape () {
     if (hand == ScissorsIndex) {
         scissors.showImage(0)
     }
-}
+    radio.sendNumber(hand)
+})
 let hand = 0
+let handplayer2 = 0
 let scissors: Image = null
 let paper: Image = null
 let rock: Image = null
 let ScissorsIndex = 0
 let paperIndex = 0
 let rockIndex = 0
+radio.setGroup(1)
 rockIndex = 1
 paperIndex = 2
 ScissorsIndex = 3
